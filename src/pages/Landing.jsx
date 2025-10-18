647968807d15459cd00f4430c6c3ee57b2ab5e4f
ranto-dev <rantoandrianandraina@gmail.com>
2025-10-18 19:42:58 +0200

    mon premier commit

---
+++ src/pages/Landing.jsx
@@ -0,0 +1,17 @@
+import Navbar from "../components/Navbar.jsx";
+import Hero from "../components/Hero.jsx";
+import Highlights from "../components/Highlights.jsx";
+import Model from "../components/IPhone.jsx";
+
+const Landing = () => {
+    return (
+      <>
+        <Navbar />
+        <Hero />
+        <Highlights />
+        <Model />
+      </>
+    );
+}
+
+export default Landing
